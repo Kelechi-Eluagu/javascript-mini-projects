8cb79e072942ea82097edd5af7f84ee30e19355d
naijatechgirle <kellyeluagu@gmail.com>
2023-04-07 20:03:32 +0100

    implemented clear input feature

--- to-do-list/to-do-list.js
+++ to-do-list/to-do-list.js
@@ -4,25 +4,42 @@ let taskInput = document.getElementById('taskInput').innerHTML;
 let doneBtn = document.getElementById('doneBtn');
 let delBtn = document.getElementById('delBtn');
 let divTask = document.getElementById('divTask');
+let valueTask = document.getElementById('today-task');
 
-let taskArr = [];
+// let taskArr = [];
 
 let allTasks = () => addBtn.addEventListener('click', addTask);
 
 function addTask() {
- // console.log(e);
- let allTaskText = taskInput;
- console.log(allTaskText);
-
+ // console.log();
+ // let allTaskText = taskInput;
+ // console.log(allTaskText);
  // taskArr.push(allTaskText);
-
  // (tasksText).value = '';
-
  // console.log(taskArr)
-
  // divTask.style.display = 'visible';
-
  // console.log(divTask)
+ let value = valueTask.value
+
+ // divTask.style.display = 'block';
+ // divTask.style.verticalAlign = 'super';
+
+ // taskInput.value = value;
+
+ console.log(value);
+ let valPlace = valueTask.placeholder;
+ console.log(valPlace);
+
+ setTimeout(() => {
+  
+  value = valPlace;
+  valueTask.value = ''
+  
+  console.log(value);
+  console.log(valPlace);
+  console.log(valueTask.value);
+  
+ }, 1000);
 }
 
 allTasks();
