9f4274c45b71d1e782561e513ca22e5a9f3f42e2
naijatechgirle <kellyeluagu@gmail.com>
2023-04-09 23:54:23 +0100

    started implementation of creating new elements

--- to-do-list/to-do-list.js
+++ to-do-list/to-do-list.js
@@ -6,34 +6,26 @@ let delBtn = document.getElementById('delBtn');
 let divTask = document.getElementById('divTask');
 let valueTask = document.getElementById('today-task');
 
-// let taskArr = [];
-
 let allTasks = () => addBtn.addEventListener('click', addTask);
 
 function addTask() {
  let value = valueTask.value;
-
- // taskInput.value = value;
-
- // let valPlace = valueTask.placeholder;
- // console.log(valPlace);/
 
  if (value.trim().length === 0 && typeof value !== Number) {
   console.log('please type a task');
  } else {
   let valueTasks = valueTask.value;
 
-  //timeout for the value of the task input to refresh
-  // setTimeout(() => {
-
   valueTask.value = '';
   divTask.classList.remove('d-none');
-  // document.createElement('divTask');
+  let newRow = document.createElement('div')
+  newRow.classList.add('row')
+  // newRow.innerHtml = 
   taskInput.value = valueTasks;
-
-  // }, 1000);
  }
 }
+
+
 
 //done button
 let doneFunction = () =>
@@ -41,23 +33,23 @@ let doneFunction = () =>
   if (doneBtn.innerText === 'Done✅') {
    doneBtn.classList.add('bg-danger');
    taskInput.classList.add('bg-white');
-   doneBtn.innerText = 'Edit';
+   doneBtn.innerText = 'Edit✏️';
    taskInput.disabled = true;
   } else {
    taskInput.disabled = false;
    doneBtn.classList.remove('bg-danger')
    doneBtn.classList.add('bg-success');
-   doneBtn.innerText = 'Done';
-   console.log('I am inside an if stateent')
+   doneBtn.innerText = 'Done✅';
   }
  });
 
-//delete button
+//delete button function
 let deleteBtn = () => delBtn.addEventListener('click', delFunction);
 
 function delFunction() {
  divTask.classList.add('d-none');
 }
+
 //this code block adds the onclick event on the html page
 // let delFunction = function(){
 //  divTask.classList.add('d-none');
